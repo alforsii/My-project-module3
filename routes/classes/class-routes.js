b67f94d7bb9365766adc56935a9ab223953ad552
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2020-04-20 17:53:53 -0400

    add GET current class students route

--- routes/classes/class-routes.js
+++ routes/classes/class-routes.js
@@ -10,28 +10,39 @@ const Teacher = require('../../models/Teacher.model')
 //Get current teachers/students all classes
 //-=-==-=--=-=-=-=-=--=-=-=-=---=-==--=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=
 router.get('/', (req, res) => {
-  getAllClasses(req.user._id, res)
+  getAllClasses(req.user._id, req, res)
 });
 //Get all classes function from users classes list
-function getAllClasses(id, res){
+function getAllClasses(id,req, res){
   User.findById(id)
+  .populate({
+    path: 'archive',
+    populate: [{ path: 'author'}, { path: 'students', populate: [{ path: 'student'}]}]
+  })
   .populate({
     path: 'classes',
     populate: [{ path: 'author'}, { path: 'students', populate: [{ path: 'student'}]}]
   })
     .then(userFromDB => {
-      const updatedClasses = userFromDB.classes.map(eachClass => {
-        eachClass.author.password = undefined
-        eachClass.students = eachClass.students.map(data => {
-          data.student.password = undefined
-          return data
-        })
-        return eachClass
-      })
-      res.status(200).json({ classes: updatedClasses });
+      const updatedClasses = removeUsersPassword(userFromDB.classes, req)
+      const updatedArchiveClasses = removeUsersPassword(userFromDB.archive, req)
+      res.status(200).json({ classes: updatedClasses, removedClasses: updatedArchiveClasses });
     })
     .catch(err => console.log(`Error while getting all classes ${err}`));
 }
+//update function - remove/hide users password === undefined
+//and filter out current student
+function removeUsersPassword(classes, req) {
+  return classes.map(eachClass => {
+    eachClass.author.password = undefined
+    eachClass.students = eachClass.students.map(data => {
+      data.student.password = undefined
+      return data
+    }).filter(data => data.student._id.toString() !== req.user._id.toString())
+    return eachClass
+  })
+}
+
 //-=-==-=--=-=-=-=-=--=-=-=-=---=-==--=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=
 //Create a class
 //-=-==-=--=-=-=-=-=--=-=-=-=---=-==--=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=
@@ -98,6 +109,80 @@ router.post('/remove-class', (req, res) => {
 
 });
 //-=-==-=--=-=-=-=-=--=-=-=-=---=-==--=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=
+//Get current class students
+//-=-==-=--=-=-=-=-=--=-=-=-=---=-==--=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=
+router.get('/:classId/class-students', (req,res) => {
+  const { classId } = req.params
+  Class.findById(classId)
+  .populate('author')
+  .populate({
+    path: 'students',
+    populate: [{ path: 'student'}]
+  })
+  .then(classFromDB => {
+  // console.log("Output for: classFromDB", classFromDB)
+    const studentsData = removeUsersPassword([classFromDB], req)[0]
+    res.status(200).json({studentsData})
+  })
+  .catch(err => console.log(`Error while getting current class students ${err}`))
+})
+//-=-==-=--=-=-=-=-=--=-=-=-=---=-==--=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=
+//Get current class teachers ------------------- not done
+//-=-==-=--=-=-=-=-=--=-=-=-=---=-==--=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=
+router.get('/:classId/class-teachers', (req,res) => {
+  const { classId } = req.params
+  Class.findById(classId)
+  .populate('author')
+  .populate({
+    path: 'students',
+    populate: [{ path: 'student'}]
+  })
+  .then(classFromDB => {
+  // console.log("Output for: classFromDB", classFromDB)
+    const studentsData = removeUsersPassword([classFromDB], req)[0]
+    res.status(200).json({studentsData})
+  })
+  .catch(err => console.log(`Error while getting current class students ${err}`))
+})
+//-=-==-=--=-=-=-=-=--=-=-=-=---=-==--=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=
+//Get all students except current class students 
+//-=-==-=--=-=-=-=-=--=-=-=-=---=-==--=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=
+router.get('/:classId/other-students', (req,res) => {
+  const { classId } = req.params
+  Class.findById(classId)
+  .populate('students')
+  .then(classFromDB => {
+  // console.log("Output for: classFromDB", classFromDB)
+    const studentsUserIds = classFromDB.students.map(data => data.student._id)
+    User.find()
+    .then(allUsersFromDB => {
+      const otherStudents = allUsersFromDB.filter(user => user.title === 'Student' && !studentsUserIds.includes(user._id) && user._id.toString() !== req.user._id.toString())
+      res.status(200).json({ students: otherStudents })
+    })
+    .catch(err => console.log(`Error in DB ${err}`))
+  })
+  .catch(err => console.log(`Error while getting current class students ${err}`))
+})
+//-=-==-=--=-=-=-=-=--=-=-=-=---=-==--=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=
+//Get all teachers except current class teachers -------------------------- not done 
+//-=-==-=--=-=-=-=-=--=-=-=-=---=-==--=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=
+router.get('/:classId/other-teachers', (req,res) => {
+  const { classId } = req.params
+  Class.findById(classId)
+  .populate('students')
+  .then(classFromDB => {
+  // console.log("Output for: classFromDB", classFromDB)
+    const studentsUserIds = classFromDB.students.map(data => data.student._id)
+    User.find()
+    .then(allUsersFromDB => {
+      const otherStudents = allUsersFromDB.filter(user => user.title === 'Student' && !studentsUserIds.includes(user._id) && user._id.toString() !== req.user._id.toString())
+      res.status(200).json({ students: otherStudents })
+    })
+    .catch(err => console.log(`Error in DB ${err}`))
+  })
+  .catch(err => console.log(`Error while getting current class students ${err}`))
+})
+//-=-==-=--=-=-=-=-=--=-=-=-=---=-==--=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=
 //Add student to the class
 //-=-==-=--=-=-=-=-=--=-=-=-=---=-==--=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=
 router.post('/add-student', (req, res) => {
@@ -109,17 +194,17 @@ router.post('/add-student', (req, res) => {
     student: userId,
   })
     .then(newStudent => {
-      // User.findByIdAndUpdate(
-      //   userId,
-      //   { $push: { classes: classId } },
-      //   { new: true }
-      // )
-      //   .then(classAddedToStudent => {})
-      //   .catch(err =>
-      //     console.log(
-      //       `Error while adding class to the students classes list ${err}`
-      //     )
-      //   );
+      User.findByIdAndUpdate(
+        userId,
+        { $push: { classes: classId } },
+        { new: true }
+      )
+        .then(classAddedToStudent => {})
+        .catch(err =>
+          console.log(
+            `Error while adding class to the students classes list ${err}`
+          )
+        );
       Class.findByIdAndUpdate(
         classId,
         { $push: { students: newStudent._id } },
@@ -169,7 +254,20 @@ router.post('/remove-student', (req, res,) => {
       console.log(`Error while getting user students to remove a student ${err}`)
     );
 });
-
+//POST update cover image
+//=--=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-==-=-
+router.post('/update/class-image', uploadCloud.single('image'),
+    (req, res) => {
+      const { classId } =req.body
+      Class.findByIdAndUpdate(classId, {
+        path: req.file.url,
+      })
+        .then(classFromDB => {
+          res.status(200).json({class: classFromDB});
+        })
+        .catch(err => res.status(500).json({message: 'Server error'}));
+    }
+  );
 
 
 module.exports = router;
