e0854fd6a5dfc8d09e243c87bf528cec4d1326f5
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2020-05-07 18:40:33 -0400

    update auth routes before 2nd commit to heroku

--- routes/users/update-profile.js
+++ routes/users/update-profile.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const bcryptjs = require('bcryptjs')
+const saltRounds = 10
 const uploadCloud = require('../../configs/cloudinary.config');
 const User = require('../../models/User.model')
 const routeGuard = require('../../configs/route-guard.configs');
@@ -9,17 +11,43 @@ const routeGuard = require('../../configs/route-guard.configs');
 //POST update profile
 //=--=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-==-=-
 router.post('/upload-profile',routeGuard,(req, res) => {
-  const {firstName,lastName,email,password} = req.body
-  if(!firstName || !lastName || !email || !password) {
-    res.status(401).json({message: 'First name, last name, email and password are required!'});
-    return
+  const { username,firstName, lastName, email, password } = req.body;
+
+  if (!username || !firstName || !lastName || !email || !password) {
+    res.status(401).json({
+      message:
+        'All fields are mandatory! Fill all inputs and try again'
+    });
+    return;
   }
-      User.findByIdAndUpdate(req.user._id, req.body, {new: true})
-        .then(userFromDB => {
-        console.log("userFromDB", userFromDB)
-          res.status(200).json({user: userFromDB});
+
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!regex.test(password)) {
+    res.status(500).json({
+      message:
+        'Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.'
+    });
+    return;
+  }
+
+
+  bcryptjs
+    .genSalt(saltRounds)
+    .then(salt => bcryptjs.hash(password, salt))
+    .then(hashedPassword => {
+        User.findByIdAndUpdate(req.user._id, {
+        ...res.body,
+        password: hashedPassword
+      }, {new: true})
+        .then(user => {
+           res.status(200).json({message: 'Thanks! Password successfully updated!', user})
         })
-        .catch(err => console.log(err));
+    })
+    .catch(err => {
+      res.status(500).json({message: 'Sorry, something went wrong in the server while updating password.'})
+      console.log(err)
+    });
+
     }
   );
 //POST uploaded photo
